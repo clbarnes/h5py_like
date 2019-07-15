0a1577fb226c186f0a569288e25e1a7591dec4fa
Chris Barnes <barnesc@janelia.hhmi.org>
2019-07-15 11:03:03 -0400

    guess chunk function (zarr implementation)

--- h5py_like/shape_utils.py
+++ h5py_like/shape_utils.py
@@ -297,6 +297,62 @@ class IndexableArrayLike(ABC):
         return write_fn(start, item_arr)
 
 
+CHUNK_BASE = 256 * 1024  # Multiplier by which chunks are adjusted
+CHUNK_MIN = 128 * 1024  # Soft lower limit (128k)
+CHUNK_MAX = 64 * 1024 * 1024  # Hard upper limit
+
+
+def guess_chunks(shape, typesize):
+    """
+    Guess an appropriate chunk layout for a dataset, given its shape and
+    the size of each element in bytes.  Will allocate chunks only as large
+    as MAX_SIZE.  Chunks are generally close to some power-of-2 fraction of
+    each axis, slightly favoring bigger values for the last index.
+    Undocumented and subject to change without warning.
+
+    Originally implemented in zarr_ (MIT license)
+
+    .. _zarr: https://github.com/zarr-developers/zarr-python/blob/e61d6ae77f18e881be0b80e38b5366793f5a2860/zarr/util.py#L64-L112
+    """  # noqa
+
+    ndims = len(shape)
+    # require chunks to have non-zero length for all dimensions
+    chunks = np.maximum(np.array(shape, dtype="=f8"), 1)
+
+    # Determine the optimal chunk size in bytes using a PyTables expression.
+    # This is kept as a float.
+    dset_size = np.product(chunks) * typesize
+    target_size = CHUNK_BASE * (2 ** np.log10(dset_size / (1024.0 * 1024)))
+
+    if target_size > CHUNK_MAX:
+        target_size = CHUNK_MAX
+    elif target_size < CHUNK_MIN:
+        target_size = CHUNK_MIN
+
+    idx = 0
+    while True:
+        # Repeatedly loop over the axes, dividing them by 2.  Stop when:
+        # 1a. We're smaller than the target chunk size, OR
+        # 1b. We're within 50% of the target chunk size, AND
+        # 2. The chunk is smaller than the maximum chunk size
+
+        chunk_bytes = np.product(chunks) * typesize
+
+        if (
+            chunk_bytes < target_size
+            or abs(chunk_bytes - target_size) / target_size < 0.5
+        ) and chunk_bytes < CHUNK_MAX:
+            break
+
+        if np.product(chunks) == 1:
+            break  # Element size larger than CHUNK_MAX
+
+        chunks[idx % ndims] = np.ceil(chunks[idx % ndims] / 2.0)
+        idx += 1
+
+    return tuple(int(x) for x in chunks)
+
+
 def threaded_block_read(
     start: Tuple[int, ...],
     shape: Tuple[int, ...],
