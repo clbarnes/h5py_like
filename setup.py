139a06debbda081acddc3aea68926f0dc74b406e
Chris Barnes <barnesc@janelia.hhmi.org>
2019-07-18 09:30:13 -0400

    Bump version: 0.4.0 → 0.5.0

--- setup.py
+++ setup.py
@@ -27,6 +27,6 @@ setup(
     name="h5py_like",
     packages=find_packages(exclude=["tests"]),
     url="https://github.com/barnesc/h5py_like",
-    version="0.4.0",
+    version="0.5.0",
     zip_safe=False,
 )
